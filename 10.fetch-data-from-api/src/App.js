dbdce18b9ddd012b8b6291963924d896813750de
Mariana Montoya <marimontod@gmail.com>
2023-01-21 18:34:28 -0500

    Exercise 10: Fetch data from an API

--- 10.fetch-data-from-api/src/App.js
+++ 10.fetch-data-from-api/src/App.js
@@ -1,27 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [userData, setUserData] = useState({});
-  
+  const url = "https://jsonplaceholder.typicode.com/users/1";
+
+  //Other option
+  /*   const getUserData = async () => {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    setUserData(jsonData);
+  }; */
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setUserData(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   // No need to touch code below
   return (
     <div className="App">
       <h2>User Data</h2>
-      <p>
+      <p data-testid='name-id'>
         <strong>Name: </strong>{" "}
         {userData.name || "(Need to populate name here)"}
       </p>
-      <p>
+      <p data-testid='website-id'>
         <strong>Website: </strong>
         {userData.website || "(Need to populate website here)"}
       </p>
-      <p>
+      <p data-testid='email-id'>
         <strong>Email: </strong>
         {userData.email || "(Need to populate email here)"}
       </p>
-      <p>
+      <p data-testid='phone-id'>
         <strong>Phone: </strong>
         {userData.phone || "(Need to populate phone here)"}
       </p>
--- 10.fetch-data-from-api/src/App.test.js
+++ 10.fetch-data-from-api/src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("should renders initial state", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const name = screen.getByTestId("name-id");
+  const website = screen.getByTestId("website-id");
+  const email = screen.getByTestId("email-id");
+  const phone = screen.getByTestId("phone-id");
+
+  expect(name.textContent).toContain("(Need to populate name here)");
+  expect(website.textContent).toContain("(Need to populate website here)");
+  expect(email.textContent).toContain("(Need to populate email here)");
+  expect(phone.textContent).toContain("(Need to populate phone here)");
+});
+
+test("should renders data after fetch data", async () => {
+  const fakeUserData = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+  };
+  const fetchSpy = jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeUserData),
+    })
+  );
+  jest.useFakeTimers();
+  const { rerender } = render(<App />);
+
+  await act(async () => {
+    jest.runAllTimers();
+    await Promise.resolve();
+  });
+
+  expect(fetchSpy).toHaveBeenCalledWith(
+    "https://jsonplaceholder.typicode.com/users/1"
+  );
+
+  rerender(<App />);
+  const name = screen.getByTestId("name-id");
+  const website = screen.getByTestId("website-id");
+  const email = screen.getByTestId("email-id");
+  const phone = screen.getByTestId("phone-id");
+
+  expect(name.textContent).toContain(fakeUserData.name);
+  expect(website.textContent).toContain(fakeUserData.website);
+  expect(email.textContent).toContain(fakeUserData.email);
+  expect(phone.textContent).toContain(fakeUserData.phone);
 });
