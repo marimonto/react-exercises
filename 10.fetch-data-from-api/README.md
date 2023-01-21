73e7e7782009a0c0a11a12461aefdf6da6081471
Mariana Montoya <marimontod@gmail.com>
2023-01-21 18:07:44 -0500

    Exercise 10: initial state

---
+++ 10.fetch-data-from-api/README.md
@@ -0,0 +1,5 @@
+Challenge: Given the url above, make this app fetch the data and display them in the browser (Small Hint: use `React.useEffect and fetch api`)
+
+Initial: https://codepen.io/angelo_jin/pen/oNGqZpm
+Solution: https://codepen.io/angelo_jin/pen/zYEWZdW
+Video for reference: https://youtu.be/VzNNjNmbXpY
---
+++ 10.fetch-data-from-api/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [userData, setUserData] = useState({});
+  
+
+  // No need to touch code below
+  return (
+    <div className="App">
+      <h2>User Data</h2>
+      <p>
+        <strong>Name: </strong>{" "}
+        {userData.name || "(Need to populate name here)"}
+      </p>
+      <p>
+        <strong>Website: </strong>
+        {userData.website || "(Need to populate website here)"}
+      </p>
+      <p>
+        <strong>Email: </strong>
+        {userData.email || "(Need to populate email here)"}
+      </p>
+      <p>
+        <strong>Phone: </strong>
+        {userData.phone || "(Need to populate phone here)"}
+      </p>
+    </div>
+  );
+}
+
+export default App;
