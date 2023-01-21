9e07c010163a5bb2bde2a2b4cd111d982997846b
Mariana Montoya <marimontod@gmail.com>
2023-01-21 15:12:41 -0500

    Exercise 04: initial state

---
+++ 04.data-binding/src/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+function App() {
+  const [] = useState("");
+
+  return (
+    <>
+      <input type="text" placeholder="Enter Text" />
+      <p></p>
+    </>
+  );
+}
+
+export default App;
