4a20881d92118dc3089716caae32788a63539c8b
Mariana Montoya <marimontod@gmail.com>
2023-01-24 10:39:24 -0500

    Exercise 15: Initial commit

---
+++ 15.advice-generator/src/components/card/Card.js
@@ -0,0 +1,9 @@
+import "./Card.css";
+const Card = () => {
+    return (<div className="card">
+        <h2 className="advice-number">ADVICE #117</h2>
+        <quote className="advice-quote">"It is easy to sit up and take notice, what's difficult is getting up and taking action"</quote>
+    </div>)
+};
+
+export default Card;
---
+++ 15.advice-generator/src/pages/Advice.js
@@ -0,0 +1,11 @@
+import Card from "../components/card/Card";
+
+const Advice = () => {
+  return (
+    <div className="container">
+      <Card />
+    </div>
+  );
+};
+
+export default Advice;
